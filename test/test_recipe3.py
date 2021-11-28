f7e76ed0e7607aaf8fda99b5647b528412e8a0db
Vineet Vimal Chheda <89501442+Vineet2311@users.noreply.github.com>
2021-11-28 18:55:55 -0500

    Update test_recipe3.py

--- test/test_recipe3.py
+++ test/test_recipe3.py
@@ -3,3 +3,12 @@ import requests
 def test_recipe1() :
     result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian").json()
     assert len(result['recipes']) != 0
+
+def test_recipe2() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Mexican").json()
+    assert len(result['recipes']) != 0
+
+def test_recipe3() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian").json()
+    assert len(result['recipes'][0]['TotalTimeInMins']) != 0
+    
