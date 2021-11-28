2d33da92fc8b065e595700c77d033d2744cd6151
Hardik Udeshi <hardikudeshi6800@gmail.com>
2021-11-28 18:27:10 -0500

    Update test_recipe3.py

--- test/test_recipe3.py
+++ test/test_recipe3.py
@@ -2,4 +2,4 @@ import requests
 
 def test_recipe1() :
     result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian").json()
-    assert result.filters.CleanedIngredients=="Mango"
+    assert result['filters']['CleanedIngredients'] == "Mango"
