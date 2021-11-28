e8f3b8424ff86c0ed245f83bc354cca012bf504c
sal0ni <mahatmasaloni@gmail.com>
2021-11-28 18:50:26 -0500

    Update test_csv_reader.py

--- test/test_csv_reader.py
+++ test/test_csv_reader.py
@@ -1,4 +1,8 @@
-from ../Data/csv-reader import restaurant_data 
+import os
+import sys
+sys.path.append(os.path.abspath('../../'))
+
+from Recipe_Recommender.Data.csv-reader import restaurant_data 
 
 def test_restaurant_data():
     assert restaurant_data("Punjabi", "North Indian", "Punjabi Dhaba", "") == "Punjabi Dhaba"
