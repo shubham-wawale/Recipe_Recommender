cd55a34ac64f3c51c68c13a6168cfc1af014e460
Vineet Vimal Chheda <89501442+Vineet2311@users.noreply.github.com>
2021-11-28 19:22:18 -0500

    Update test_recipe2.py

--- test/test_recipe2.py
+++ test/test_recipe2.py
@@ -4,3 +4,11 @@ def test_1() :
     result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Tomato").text
     assert result.find("Tomato") != -1
 
+def test_2() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=pear").text
+    assert result.find("pear") != -1    
+def test_3() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=peach").text
+    assert result.find("peach") != -1
+
+    
