585f6be3eaf6f49fe7e84214726cebafc44f1343
Vineet Vimal Chheda <89501442+Vineet2311@users.noreply.github.com>
2021-11-28 19:54:13 -0500

    Update test_TransIngred.py

--- test/test_TransIngred.py
+++ test/test_TransIngred.py
@@ -1,4 +1,5 @@
 import requests
+
 def test() :
-    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Mexican").json()
-    assert len(result['TranslatedIngredients']) != 0
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian").json()
+    assert len(result['recipes'][0]['TranslatedIngredients']) != 0
