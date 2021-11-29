46c6c543ab4c7a31c65197ca981b547c76199eea
KALGEE ANAND KOTAK <43135408+kalgeekotak99@users.noreply.github.com>
2021-11-28 20:06:02 -0500

    Update test_csv_reader.py

--- test/test_csv_reader.py
+++ test/test_csv_reader.py
@@ -11,4 +11,4 @@ def test_location_data():
     assert "Raleigh" in location_data("Punjabi", "North Indian", "Raleigh", "")
 
 def test_diet_type_data_Non_Veg():
-		assert "Non-Vegetarian" in diet_type_data("Chicken")
+		assert "Non-Vegetarian" in diet_type_data("chicken")
