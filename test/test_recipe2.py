b01f5a4ac197d9ed654f3ee3fa40766d0aa39168
Vineet Vimal Chheda <89501442+Vineet2311@users.noreply.github.com>
2021-11-28 19:27:01 -0500

    Update test_recipe2.py

--- test/test_recipe2.py
+++ test/test_recipe2.py
@@ -11,4 +11,27 @@ def test_3() :
     result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=peach").text
     assert result.find("peach") != -1
 
+def test_4() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=salt").text
+    assert result.find("salt") != -1
+
+def test_5() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=sugar").text
+    assert result.find("sugar") != -1
     
+def test_6() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=cheese").text
+    assert result.find("cheese") != -1    
+def test_7() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=lemon").text
+    assert result.find("lemon") != -1
+
+def test_8() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=spinach").text
+    assert result.find("spinach") != -1
+def test_9() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=apple").text
+    assert result.find("apple") != -1
+def test_10() :
+    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=tortillas").text
+    assert result.find("tortillas") != -1    
